6275e7db56fd00453377d6e402493842b7ab8d72
Lasse Keetz <l.t.keetz@geo.uio.no>
2021-05-12 14:53:08 +0200

    Deleted nlp_cases folder from git commits.

--- nlp_cases/ALP1/.env_mach_specific.sh
+++
@@ -1,7 +0,0 @@
-# This file is for user convenience only and is not used by the model
-# Changes to this file will be ignored and overwritten
-# Changes to the environment should be made in env_mach_specific.xml
-# Run ./case.setup --reset to regenerate this file
-source /cluster/installations/lmod/lmod/init/sh
-module load StdEnv CMake/3.15.3-GCCcore-8.3.0 Perl/5.30.0-GCCcore-8.3.0 XML-LibXML/2.0201-GCCcore-8.3.0 iimpi/2019b netCDF-Fortran/4.5.2-iimpi-2019b
-export KMP_STACKSIZE=256M
