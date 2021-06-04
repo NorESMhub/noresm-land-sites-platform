80739de0eb777208e3de55982c3b4fd24f9bbf34
Emiliano Gelati <emiliano-gelati@users.noreply.github.com>
2021-06-04 20:08:16 +0200

    NREC virtual machine creation: instructions and tested setup script #27

--- NREC_VM_setup/README.md
+++ NREC_VM_setup/README.md
@@ -1,6 +1,28 @@
 # Tools to set up a NREC virtual machine for development
 
-The main script is `setup_VM_NREC.sh`.
+To create and configure a virtual machine on NREC
+(https://docs.nrec.no/index.html):
+1. Register as a user and save your API password, which you will need in step 2
+   (https://docs.nrec.no/login.html#first-time-login)
+2. Install OpenStack command line interface tools
+   (https://docs.nrec.no/api.html#openstack-command-line-interface-cli)
+   and create a `keystone_rc.sh` file in your local home folder
+   (https://docs.nrec.no/api.html#using-the-cli-tools)
+3. On your local Linux machine run the script `setup_VM_NREC.sh`, which will
+   set up the remote virtual machine and install the dependencies.
+   The following parameters are set at the beginning of the script under
+   "Default names and paths" (virtual machine name, SSH key path and
+   name, and security group name).
+   Several steps (Create security group and rules, Upload public SSH key,
+   Instance creation) can be done manually on the
+   [NREC dashboard](https://dashboard.nrec.no): links to the documentation are
+   provided in `setup_VM_NREC.sh`.
+4. Login information is printed at the end of `setup_VM_NREC.sh` execution.
 
-To do:
-- Check how to access model input, e.g. via sshfs
+To do list (in `setup_VM_NREC.sh`):
+- CLM dependencies (see [CTSM Dockerfile](https://github.com/sunnivin/docker-
+local-build-run-CTSM/blob/main/docker/baseos/centos/centos7.6/Dockerfile))
+- Create, attach and mount storage volume
+- Mount cluster storage via sshfs (needed to create data for new sites?)
+- Use shell arguments in to set machine parameters nowhard-coded under
+  "Default names and paths".
--- NREC_VM_setup/setup_VM_NREC.sh
+++ NREC_VM_setup/setup_VM_NREC.sh
@@ -1,18 +1,39 @@
-# https://docs.nrec.no/create-virtual-machine.html#doing-the-same-with-cli
-name_vm=myserver
-# Upload public SSH key
-openstack keypair create --public-key ~/.ssh/id_rsa.pub mykey
-# Instance creation from client
-openstack server create --image "GOLD Ubuntu 20.04 LTS" \
-                        --flavor m1.small \
-                        --security-group SSH_and_ICMP \
-                        --security-group default \
-                        --key-name mykey \
-                        --nic net-id=dualStack $name_vm
-# Get instance's IP address
+#! /usr/bin/bash
+set -e # Exit if any command fails
 unalias grep # only needed if grep is aliased .bashrc or similar
-address=`openstack server list | grep $name_vm | grep -oP 'dualStack=\K([0-9\.]+)'`
-echo "NREC machine IP address: $address"
+
+# Default names and paths (TO DO: use command line arguments)
+name_server=test_vm # name of virtual machine
+path_key=~/.ssh/id_rsa.pub # path of local SSH public key to be uploaded
+name_key=mykey # key name once uploaded to NREC
+security_group=SSH_ICMP_all # name of project's security group
+
+# Create security group and rules (https://docs.nrec.no/security-groups.html)
+if ! openstack security group list | grep -q "$security_group"; then
+    openstack security group create --description "Allow incoming SSH and ICMP" $security_group
+    openstack security group rule create --ethertype IPv4 --protocol icmp --remote-ip 0.0.0.0/0 $security_group
+    openstack security group rule create --ethertype IPv4 --protocol tcp --dst-port 22 --remote-ip 0.0.0.0/0 $security_group
+fi
+
+# Upload public SSH key (https://docs.nrec.no/ssh.html)
+if ! openstack keypair list | grep -q "$name_key"; then
+    openstack keypair create --public-key $path_key $name_key
+    echo "Public SSH key uploaded to NREC"
+fi
+
+# Instance creation (https://docs.nrec.no/create-virtual-machine.html)
+if ! openstack server list | grep -q $name_server; then
+    openstack server create --image "GOLD Ubuntu 20.04 LTS" \
+                            --flavor m1.small \
+                            --security-group $security_group \
+                            --security-group default \
+                            --key-name $name_key \
+                            --nic net-id=dualStack $name_server
+fi
+
+# Get virtual machine's IP address and add it to SSH known hosts
+address=`openstack server list | grep "$name_server" | grep -oP 'dualStack=\K([0-9\.]+)'`
+ssh-keyscan -H $address >> ~/.ssh/known_hosts
 
 # Install dependencies and clone repositories on virtual machine
 scp {install_*.sh,requirements_*.txt,.vimrc} ubuntu@$address:~/
@@ -20,5 +41,12 @@ ssh ubuntu@$address chmod 700 install_*.sh
 ssh ubuntu@$address ./install_dependencies.sh
 ssh ubuntu@$address ./install_platform.sh
 
-# Mount cluster storage via sshfs?
+# Create, attach and mount storage volume
 # TO DO
+
+# Mount cluster storage via sshfs (needed to create data for new sites?)
+# TO DO
+
+# Print login information
+echo "NREC machine IP address: $address"
+echo "To login, type: ssh ubuntu@$address"
