456854c4fe0a9f0a08f07318feae0b59512c24dd
Cloud User <emiliano-gelati@users.noreply.github.com>
2021-07-05 16:40:42 +0000

    Restructure settings file and parser class for base workflow #15 #25

--- landsites_tools/interface_settings.py
+++ landsites_tools/interface_settings.py
@@ -5,220 +5,83 @@ import pandas as pd
 from dateutil.parser import parse as parsetime
 
 
-def split_sequence(sequence, sep_regex='[ ,;]+'):
-    """Interpret sequence of values as list
-    Arguments:
-        sequence: string with sequence of separable values
-        sep_regex: regular expression patter used for separation
-    Returns:
-        list of values
+def interactive_settings():
     """
-    return re.split('[ ,;]+', sequence)
+    Example workflow:
+    1. get settings from command-line user input
+    2. create ConfigParser instance with settings formatted as
+       in the default settings file
+    3. write settings file
+    (https://docs.python.org/3.6/library/configparser.html#configparser-objects)
+    """
+    raise NotImplementedError
 
 
-class InterfaceSettings:
-    """Interface to the settings of NorESM Land Sites Platform"""
+class SettingsParser:
+    """NorESM Land Sites Platform settings parser"""
 
-    def __init__(self, pathsettings):
+    def __init__(self, path_settings):
         """Parse the NorESM Land Site Platform settings file
         Arguments:
-            pathsettings: path to settings file
+            paths_settings: path to settings file
         Returns:
-            instance of LandSiteInput class
+            instance of SettingsParser class
         """
-        # Path of original settings file
-        self.path_original_settings = pathsettings
-        # Load settings into a ConfigParser instance
-        self.parser = ConfigParser(interpolation=ExtendedInterpolation(),
-                                      inline_comment_prefixes='#')
-        self.parser.read(self.path_original_settings)
-
-        # Platform directory
-        self.dir_platform = Path(__file__).absolute().parent.parent
-
-        self.input_dir = self.dir_platform / \
-        self.read_path('paths_basic', 'input_dir')
-
-        self.output_dir = self.dir_platform / \
-        self.read_path('paths_basic', 'output_dir')
-
-        # Names and coordinates of all available sites
-        self.path_sites_table = self.dir_platform / \
-        self.read_path('paths_advanced', 'sites_table')
-
-
-        try:
-            with open(self.path_sites_table, "r") as sites_json:
-                self.info_all_sites = pd.read_json(sites_json,
-                                                   orient='index')
-        except Exception as error:
-            print("\nError when reading specified site info .json file!\n")
-            raise error
-
+        # Default information
+        self.dir_platform = Path(__file__).absolute().parents[1]
+        self.path_sites_table = self.dir_platform / 'data' / 'sites.json'
         self.info_all_sites = pd.read_json(self.path_sites_table,
                                            orient='index')
-        print(self.info_all_sites)
+        # Load user settings into a ConfigParser instance (low-level interface)
+        self.interface = ConfigParser(interpolation=ExtendedInterpolation(),
+                                      inline_comment_prefixes='#')
+        self.interface.read(path_settings)
+        # Parse user settings to convenient types
+        self.dir_cases = self.get_path('path', 'dir_cases')
+        self.dir_input = self.get_path('path', 'dir_input')
+        self.dir_output = self.get_path('path', 'dir_output')
+        self.start_time = self.get_time('run', 'start_time')
+        self.end_time = self.get_time('run', 'end_time')
+        self.sites2run = self.get_sequence('run', 'sites2run')
+        self.type_run = self.get_parameter('run', 'type_run')
+        self.type_model = self.get_parameter('run', 'type_model')
+        self.initial_file = self.get_parameter('run', 'initial_file')
+        self.frequency_output = self.get_parameter('run', 'frequency_output')
+        self.variables_output = self.get_sequence('run', 'variables_output')
+        self.variables_plot = self.get_sequence('postprocess', 'variables_plot')
+        self.frequency_plot = self.get_parameter('postprocess', 'frequency_plot')
 
-        # Directory of raw NorESM input data
-        self.input_raw = self.read_path('paths_advanced', 'input_raw')
-        # Directory of NorESM code and script paths
-        self.dir_clm_tools = self.read_path('scripts', 'dir_clm_tools')
-        self.path_mknoocnmap = self.read_path('scripts', 'mknoocnmap')
-
-    ############################################################################
-    '''
-    Basic user settings.
-    '''
-
-    @property
-    def platform_dir(self):
-        """Input-related flow control switches"""
-        return self.dir_platform
-
-    @property
-    def input_dir(self):
-        """Root directory of data to be created"""
-        return self.input_dir
-
-    @property
-    def output_dir(self):
-        """Root directory of data to be created"""
-        return self.output_dir
-
-    @property
-    def sites_df(self):
-        """Input-related flow control switches"""
-        return self.info_all_sites
-
-    ############################################################################
-    '''
-    Case run options. Needed?
-    '''
-    '''
-    ### Sites and version of input data ###
-    @property
-    def sites2run(self):
-        """Sites to be simulated"""
-        return split_sequence(self.parser['case_run_params']['sites2run'])
-
-    @property
-    def version(self):
-        """Version of data to be used or created."""
-        return self.parser['case_run_params']['version']
-
-    ### Simulation mode ###
-    @property
-    def run_type(self):
-        """Defines the mode to run (e.g. from existing restart file)."""
-        return self.parser['case_run_params']['run_type']
-
-    @property
-    def continue_from_restart_file(self):
-        """Switch to start the simulation from an existing restart file."""
-        return self.parser['case_run_params']['continue_from_restart_file']
-
-    @property
-    def run_ref_case(self):
-        """Name of the reference case (for restart and branch runs)."""
-        return self.parser['case_run_params']['run_ref_case']
-
-    ### Simulation period ###
-    @property
-    def run_start_date(self):
-        """Start date for simulation period."""
-        return self.parser['case_run_params']['run_start_date']
-
-    @property
-    def n_years(self):
-        """Number of years to simulate per resubmit."""
-        return self.parser['case_run_params']['n_years']
-
-    @property
-    def n_resubmit(self):
-        """Number of resubmits.
-        Total years = n_years + (n_years*n_resubmit)."""
-        return self.parser['case_run_params']['n_resubmit']
-
-    @property
-    def end_date(self):
-        """End date for simulation period (in conjunction with n_years and
-        n_resubmit)."""
-        return self.parser['case_run_params']['end_date']
-
-    @property
-    def calendar(self):
-        """Calendar setting to use (includes or excludes leap years)."""
-        return self.parser['case_run_params']['calendar']
-
-    ### High Performance Computing (HPC) settings ###
-    @property
-    def job_time_hpc(self):
-        """Job wallclock time for hpc simulations. Choose carefully!"""
-        return self.parser['case_run_params']['job_time_hpc']
-
-    @property
-    def machine_hpc(self):
-        """Name representing the HPC machine settings."""
-        return self.parser['case_run_params']['machine_hpc']
-
-    @property
-    def project_hpc(self):
-        """Name of the project associated with a simulation run."""
-        return self.parser['case_run_params']['project_hpc']
-    '''
-
-    ############################################################################
-    '''
-    FATES parameters.
-    '''
-    '''
-    @property
-    def exclude_pft_indices(self):
-        """Sites to be simulated"""
-        return self.parser['fates_params']['exclude_pft_indices']
-    '''
-    ############################################################################
-    '''
-    Helper functions.
-    '''
-
-    def get_param_value(self, section_name, param_name):
+    def get_parameter(self, section_name, param_name):
         '''Returns the value for a requested parameter string.'''
-        return self.parser[section_name][param_name]
+        return self.interface[section_name][param_name]
 
-    def read_path(self, section, parameter):
-        """Read and parse path from settings (configparser.ConfigParser format)
+    def get_path(self, section, parameter):
+        """Read and parse path from settings low-level interface
         Arguments:
             section: settings file section
             parameter: settings file parameter name
-        """
-        return Path(self.parser[section][parameter]).expanduser()
-
-    def read_switches(self, section):
-        """Read all switches of a section as dictionary entries
-        Arguments:
-            section: settings file section with True/False switches
-        """
-        return {k: eval(s) for k, s in self.parser.items(section)}
-
-    def case_name(self, name_site):
-        """Create case name
-        Arguments:
-            name_site: name of the site to be simulated
         Returns:
-            case name formatted with version number
+            path
         """
-        return f"{name_site}_default_{self.version}"
+        return Path(self.get_parameter(section, parameter)).expanduser()
 
-    def write_file(self, path):
-        """Writes (modified) settings to a file. It avoids overwriting
-        the original settings file
+    def get_sequence(self, section, parameter, sep_regex='[ ,;]+'):
+        """Read and parse sequence of values as list
         Arguments:
-            path: output path
+            section: settings file section
+            parameter: settings file parameter name
+            sep_regex: regular expression patter used for separation
+        Returns:
+            list of values
         """
-        if path != self.path_original_settings:
-            self.parser.write(open(path, 'w'))
-            print(f"Modified settings written to {path}")
-        else:
-            raise Exception("Cannot overwrite original settings file"\
-                            f" {self.path_original_settings}")
+        return re.split(sep_regex, self.get_parameter(section, parameter))
+
+    def get_time(self, section, parameter):
+        """Read and parse time information as datetime.datetime
+        Arguments:
+            section: settings file section
+            parameter: settings file parameter name
+        Returns:
+            datetime.datetime instance
+        """
+        return parsetime(self.get_parameter(section, parameter))
