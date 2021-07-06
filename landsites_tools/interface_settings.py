9357ccb6e3f86d08dc661c8124e99ecc32e57e0c
Cloud User <emiliano-gelati@users.noreply.github.com>
2021-07-06 15:35:42 +0000

    Settings validation checks #25

--- landsites_tools/interface_settings.py
+++ landsites_tools/interface_settings.py
@@ -1,7 +1,9 @@
 import re
+import json
 from configparser import ConfigParser, ExtendedInterpolation
 from pathlib import Path
 import pandas as pd
+import numpy as np
 from dateutil.parser import parse as parsetime
 
 
@@ -27,15 +29,20 @@ class SettingsParser:
         Returns:
             instance of SettingsParser class
         """
+        # Settings file path
+        self.path_settings = Path(path_settings).absolute()
         # Default information
         self.dir_platform = Path(__file__).absolute().parents[1]
-        self.path_sites_table = self.dir_platform / 'data' / 'sites.json'
+        self.dir_info = self.dir_platform / 'data'
+        self.path_sites_table = self.dir_info / 'sites.json'
         self.info_all_sites = pd.read_json(self.path_sites_table,
                                            orient='index')
+        self.path_constraints = self.dir_info / 'settings_constraints.json'
+        self.constraints = json.load(open(self.path_constraints, 'r'))
         # Load user settings into a ConfigParser instance (low-level interface)
         self.interface = ConfigParser(interpolation=ExtendedInterpolation(),
                                       inline_comment_prefixes='#')
-        self.interface.read(path_settings)
+        self.interface.read(self.path_settings)
         # Parse user settings to convenient types
         self.dir_cases = self.get_path('path', 'dir_cases')
         self.dir_input = self.get_path('path', 'dir_input')
@@ -50,10 +57,20 @@ class SettingsParser:
         self.variables_output = self.get_sequence('run', 'variables_output')
         self.variables_plot = self.get_sequence('postprocess', 'variables_plot')
         self.frequency_plot = self.get_parameter('postprocess', 'frequency_plot')
+        # Additional settings validation
+        self._check_sites()
 
     def get_parameter(self, section_name, param_name):
-        '''Returns the value for a requested parameter string.'''
-        return self.interface[section_name][param_name]
+        '''Returns the value for a requested parameter string,
+        raising an exception if any value is invalid.'''
+        value = self.interface[section_name][param_name]
+        if param_name in self.constraints.keys():
+            allowed = self.constraints[param_name]
+            if value not in allowed:
+                msg = f"Invalid value '{value}' for setting '{param_name}'." +\
+                      f" Valid values: {allowed} (see {self.path_constraints})"
+                raise ValueError(msg)
+        return value
 
     def get_path(self, section, parameter):
         """Read and parse path from settings low-level interface
@@ -85,3 +102,12 @@ class SettingsParser:
             datetime.datetime instance
         """
         return parsetime(self.get_parameter(section, parameter))
+
+    def _check_sites(self):
+        "Raise an exception if site names are not valid"
+        invalid = np.setdiff1d(self.sites2run, self.info_all_sites.index)
+        if invalid.size:
+            msg = f"{invalid.tolist()} are invalid sites. " +\
+                  f"Valid sites: {self.info_all_sites.index.tolist()} " +\
+                  f"(see {self.path_sites_table})"
+            raise ValueError(msg)
