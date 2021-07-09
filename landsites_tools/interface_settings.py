2829fa2844f32d38980706c58b6deb3820cdaf5f
Cloud User <centos@lasse-dev.novalocal>
2021-07-09 21:34:35 +0000

    Updated project structure. Added working version for interactively creating settings files. Not finished, backup commit.

--- landsites_tools/interface_settings.py
+++ landsites_tools/interface_settings.py
@@ -1,5 +1,6 @@
 import re
 import json
+import csv
 from configparser import ConfigParser, ExtendedInterpolation
 from pathlib import Path
 import pandas as pd
@@ -23,67 +24,148 @@ class SettingsParser:
     """NorESM Land Sites Platform settings parser"""
 
     def __init__(self, path_settings):
-        """Parse the NorESM Land Site Platform settings file
+        """
+        Parse the NorESM Land Site Platform settings file
         Arguments:
             paths_settings: path to settings file
         Returns:
             instance of SettingsParser class
         """
-        # Settings file path
+        ### Load user settings into a ConfigParser instance (low-level interface)
+        self.interface = ConfigParser(interpolation=ExtendedInterpolation(),
+                                      inline_comment_prefixes='#')
+        # Path to settings file
         self.path_settings = Path(path_settings).absolute()
-        # Default information
+        self.interface.read(self.path_settings)
+
+        ### Default information
         self.dir_platform = Path(__file__).absolute().parents[1]
-        self.dir_info = self.dir_platform / 'data'
+        self.dir_info = self.dir_platform / 'data' / '.nlp'
         self.path_sites_table = self.dir_info / 'sites.json'
         self.info_all_sites = pd.read_json(self.path_sites_table,
                                            orient='index')
         self.path_constraints = self.dir_info / 'settings_constraints.json'
-        self.constraints = json.load(open(self.path_constraints, 'r'))
-        # Load user settings into a ConfigParser instance (low-level interface)
-        self.interface = ConfigParser(interpolation=ExtendedInterpolation(),
-                                      inline_comment_prefixes='#')
-        self.interface.read(self.path_settings)
-        # Parse user settings to convenient types
-        self.dir_cases = self.get_path('path', 'dir_cases')
-        self.dir_input = self.get_path('path', 'dir_input')
-        self.dir_output = self.get_path('path', 'dir_output')
+
+        ### Read parameter dictionary
+        with open(self.path_constraints, 'r') as constraints_file:
+            self.constraints = json.load(constraints_file)
+
+        ### Parse user settings to convenient types
+        # Paths
+        self.dir_cases = self.get_path('path', 'dir_cases', 'dir')
+        self.dir_input = self.get_path('path', 'dir_input', 'dir')
+        self.dir_input_clmforc = \
+        self.dir_input / "inputdata" / "atm" / "datm7" / "GSWP3v1" # TO-DO
+        self.dir_output = self.get_path('path', 'dir_output', 'dir')
+        # Dates
         self.start_time = self.get_time('run', 'start_time')
         self.end_time = self.get_time('run', 'end_time')
-        self.sites2run = self.get_sequence('run', 'sites2run')
-        self.type_run = self.get_parameter('run', 'type_run')
-        self.type_model = self.get_parameter('run', 'type_model')
-        self.initial_file = self.get_parameter('run', 'initial_file')
-        self.frequency_output = self.get_parameter('run', 'frequency_output')
-        self.variables_output = self.get_sequence('run', 'variables_output')
-        self.variables_plot = self.get_sequence('postprocess', 'variables_plot')
-        self.frequency_plot = self.get_parameter('postprocess', 'frequency_plot')
-        # Additional settings validation
+        self.n_simulation_days = self.get_n_days(self.start_time, self.end_time)
+        # Sites
+        self.sites2run = self.get_sites('run', 'sites2run')
         self._check_sites()
+        # COMPSET
+        self.compset_str = self._generate_compset_string() # TO-DO
+        # MACHINE
+        self.machine_str = "container-nlp" # TO-DO, SHOULDN'T BE HARD-CODED!
+        # Other
+        self.type_run = self.read_parameter('run', 'type_run')
+        self.type_model = self.read_parameter('run', 'type_model')
+        self.initial_file = self.read_parameter('run', 'initial_file')
+        self.frequency_output = self.read_parameter('run', 'frequency_output')
+        self.variables_output = self.read_parameter('run', 'variables_output')
+        self.variables_plot = self.read_parameter('postprocess', 'variables_plot')
+        self.frequency_plot = self.read_parameter('postprocess', 'frequency_plot')
 
-    def get_parameter(self, section_name, param_name):
-        '''Returns the value for a requested parameter string,
-        raising an exception if any value is invalid.'''
+
+    def write_file(self, path):
+        """
+        Write instance to a new settings file.
+        """
+        if self._is_valid_path(path, type="file", can_create=True):
+            self.interface["run"]["sites2run"] = self.get_parameter('sites2run')
+            # Writing our configuration file to 'example.cfg'
+            with open(path, 'w') as configfile:
+                self.interface.write(configfile)
+
+            return True
+        else:
+            return False
+    ############################################################################
+    def read_parameter(self, section_name, param_name):
+        '''
+        Reads and returns the value for a requested parameter string from the
+        settings file, raising an exception if any value is invalid.
+        '''
         value = self.interface[section_name][param_name]
+
+        ### Are constraints defined?
         if param_name in self.constraints.keys():
-            allowed = self.constraints[param_name]
-            if value not in allowed:
-                msg = f"Invalid value '{value}' for setting '{param_name}'." +\
-                      f" Valid values: {allowed} (see {self.path_constraints})"
-                raise ValueError(msg)
+            try:
+                ### Cast to correct Python object type
+                cur_type = eval(self.constraints[param_name].get("type"))
+
+                try:
+                    # If list is expected, convert by splitting entries
+                    if isinstance(cur_type, list):
+                        # Must be seperated by comma, semicolon, or whitespace
+                        value = re.split("[ ,;]+", value)
+                    else:
+                        value = cur_type(value)
+                except:
+                    raise TypeError(f"'{value}' must be of type '{cur_type}'!")
+
+                self._is_valid_param_value(param_name, value)
+
+            except:
+                raise
+
         return value
 
-    def get_path(self, section, parameter):
-        """Read and parse path from settings low-level interface
+    def get_parameter(self, param_name):
+        '''
+        Returns the requested parameter.
+        '''
+        return getattr(self, param_name)
+
+    def set_parameter(self, param_name, value):
+        """
+        Set an instance parameter to a new value.
+        """
+        try:
+            self._is_valid_param_value(param_name, value)
+
+            setattr(self, param_name, value)
+
+            return True
+        except:
+            raise
+
+
+    ############################################################################
+    def get_path(self, section, parameter, type="dir"):
+        """
+        Read and parse path from settings low-level interface
         Arguments:
             section: settings file section
             parameter: settings file parameter name
+            type: object the path points to ('dir' or 'file')
         Returns:
             path
         """
-        return Path(self.get_parameter(section, parameter)).expanduser()
+        try:
+            path = Path(self.read_parameter(section, parameter)).expanduser()
+            self._is_valid_path(path, type=type)
 
-    def get_sequence(self, section, parameter, sep_regex='[ ,;]+'):
-        """Read and parse sequence of values as list
+            return path
+        except:
+            ### Ask user to create new folder?
+            raise
+
+    ############################################################################
+    def get_sites(self, section, parameter, sep_regex='[ ,;]+'):
+        """
+        Read and parse sequence of values as list
         Arguments:
             section: settings file section
             parameter: settings file parameter name
@@ -91,23 +173,137 @@ class SettingsParser:
         Returns:
             list of values
         """
-        return re.split(sep_regex, self.get_parameter(section, parameter))
+        sites_raw_str = self.interface[section][parameter]
 
+        return re.split(sep_regex, sites_raw_str)
+
+    ############################################################################
     def get_time(self, section, parameter):
-        """Read and parse time information as datetime.datetime
+        """
+        Read and parse time information as datetime.datetime
         Arguments:
             section: settings file section
             parameter: settings file parameter name
         Returns:
             datetime.datetime instance
         """
-        return parsetime(self.get_parameter(section, parameter))
+        return parsetime(self.read_parameter(section, parameter))
+
+    def get_n_days(self, start_date, end_date):
+        """
+        Calculates the number of days for model run.
+        Arguments:
+            start_date: datetime object for model start date
+            end_date: datetime object for model end date
+        Returns:
+            Simulation length in days as int.
+        """
+        return (end_date-start_date).days
+
+
+    ############################################################################
+    ############################################################################
+    ############################################################################
 
     def _check_sites(self):
-        "Raise an exception if site names are not valid"
+        '''
+        Raise an exception if site names are not valid
+        '''
         invalid = np.setdiff1d(self.sites2run, self.info_all_sites.index)
         if invalid.size:
             msg = f"{invalid.tolist()} are invalid sites. " +\
                   f"Valid sites: {self.info_all_sites.index.tolist()} " +\
                   f"(see {self.path_sites_table})"
             raise ValueError(msg)
+
+    ############################################################################
+    def _is_valid_path(self, path, type="dir", can_create=False):
+        '''
+        Check if a given directory or file exists. Returns False if the input is
+        an empty string, True if it is a valid directory, and raises an
+        exception if given path is not valid.
+        '''
+        if type not in ["dir", "file"]:
+            raise ValueError("'type' must be either 'dir' or 'file'!")
+
+        ### Return False if no string given
+        if path == "":
+            return False
+
+        if not str(path).startswith(str(self.dir_platform)):
+            path = Path(self.dir_platform / path)
+
+        ### If not empty string, check if path is valid
+        try:
+            if type == "dir":
+                if not Path(path).is_dir():
+                    raise ValueError(f"Directory {path} does not exist!")
+            elif type == "file":
+                if can_create:
+                    if Path(path).parent.is_dir():
+                        return True
+                    else:
+                        raise ValueError(f"Directory {Path(path).parent} does not exist!")
+                else:
+                    if not Path(path).is_file():
+                        raise ValueError(f"File {path} does not exist!")
+            ### Return true if no exceptions
+            return True
+        except:
+            raise
+
+    ############################################################################
+    def _is_valid_param_value(self, param_name, value):
+        '''
+        Validate the paramaters given in the settings file.
+        Raises an exception if input is erroneous.
+        '''
+
+        if param_name not in self.constraints.keys():
+            # TO-DO: smarter way to handle exceptions
+            return True
+
+        ### Check if the input is in valid entries
+        # First check if valid values are stored in a seperate file
+        if int(self.constraints[param_name].get("valid_in_txt_file")):
+            # Read that file and save values in list
+            with open(self.dir_info / self.constraints[param_name]. \
+            get("valid_values")) as f:
+                s_file = csv.reader(f, delimiter=",")
+                valid_vals = []
+                for row in s_file:
+                    valid_vals.extend(row)
+        # Otherwise, use values directly from dict
+        else:
+            valid_vals = self.constraints[param_name].get("valid_values")
+
+        ### Test if provided values are valid
+        try:
+            if isinstance(value, list):
+                # All items in provided parameter list also in the valid list?
+                if any(item in value for item in valid_vals):
+                    raise ValueError()
+            else:
+                # Provided parameter in valid list?
+                if value not in valid_vals:
+                    raise ValueError()
+        # Catch exceptions
+        except:
+            # Raise value exception
+            msg = f"Invalid value '{value}' for setting '{param_name}'. " +\
+                  f"Valid values: {valid_vals} (see the documentation)."
+
+            raise ValueError(msg)
+
+        # If no exceptions were raised, return True
+        return True
+
+    ############################################################################
+
+    def _generate_compset_string(self):
+        """
+        Create and store a compset string based on provided input.
+
+        TO-DO: Implement different modes.
+        """
+        return "2000_DATM%1PTGSWP3_CLM50%FATES_SICE_SOCN_MOSART_SGLC_SWAV"
--- landsites_tools/simulations/helpers.py
+++ landsites_tools/simulations/helpers.py
@@ -1,7 +1,98 @@
 ### Imports
 from pathlib import Path
 
-def download_input_data(case_name, version, url, home_dir):
+################################################################################
+
+def change_case_parameters(case_path, interface_settings, param_dict):
+
+    ### Initialize bash command to change settings in current case folder
+    bash_command = f"cd {case_path};"
+
+    try:
+
+        ### Loop through parameter keys and add value change to bash cmd
+        # If no parameter stored in interface object, use default value
+        bash_command += \
+        ";".join(
+            [f"./xmlchange --file {vals['xml_file']} --id {vals['ctsm_param']}"\
+            + f" --val {interface_settings.get_parameter(nlp_param)}" \
+            if interface_settings.get_parameter(nlp_param) is not None else \
+            f"./xmlchange --file {vals['xml_file']} --id {vals['ctsm_param']}"\
+            + f" --val {vals['default']}" \
+            for nlp_param,vals in param_dict['user'].items()]
+        )
+
+        ### Also add general settings
+        bash_command += \
+        ";".join(
+            [f"./xmlchange --file {vals['xml_file']} --id {ctsm_param} " \
+            + f"--val {vals['value']}" \
+            for ctsm_param,vals in param_dict['nlp_general'].items()]
+        )
+
+        ### Execute bash
+        subprocess.run(bash_command, shell=True, check=True)
+
+        print(f"Parameters successfully changed in '{case_path}'.")
+
+    except:
+        print("Error when changing case parameters!\n")
+        raise
+
+
+################################################################################
+
+def _case_dir_exists(case_dir_path):
+    """
+    Check if the case directory already exists.
+
+    To-do: make safer (e.g. check if dir contains necessary files).
+    """
+    # Return True or False
+    return Path(case_dir_path).is_dir()
+
+def create_case(case_dir_name, nlp_case_name, dir_platform,
+                dir_cases, compset_str, machine_str):
+    """
+    Create CTSM cases.
+    """
+
+    case_dir_path = Path(dir_cases) / case_dir_name
+
+    try:
+        if _case_dir_exists(case_dir_path):
+            print(f"Case '{case_dir_name}' already exists in the specified "\
+            + f"cases directory.")
+        else:
+            ### Concatenate a bash string to create CTSM case
+            bash_command = \
+            f"{dir_platform}/noresm2/cime/scripts/create_newcase " \
+            + f"--case {case_dir_path} --compset {compset_str} " \
+            + f"--res 1x1_{nlp_case_name} --machine {machine_str} " \
+            + f"--run-unsupported"
+
+            subprocess.run(bash_command, shell=True, check=True)
+
+            print(f"New case '{case_dir_name}' successfully created.")
+
+    except:
+        print("Error when creating cases!\n")
+        raise
+
+
+################################################################################
+def _input_exists(input_dir_path):
+    """
+    Check if the input data directory exists for a case name exists.
+
+    To-do: make safer (e.g. check if dir contains necessary forcings).
+    """
+
+    # Return True or False
+    return Path(input_dir_path).is_dir()
+
+
+def download_input_data(case_name, version, url, input_dir):
     '''
     Downloads CLM input data from given URL, e.g. defined in 'nlp_sites.json'.
     case_name (str): name of case
@@ -13,36 +104,41 @@ def download_input_data(case_name, version, url, home_dir):
     import shutil
     import tarfile
 
-    file_name = case_name + "_" + str(version)
-    file_path = home_dir / "data" / "input" / file_name
+    case_input_dir_name = case_name + "_" + str(version)
+    case_input_dir_path = input_dir / case_input_dir_name
 
     ### Check if input directory already exists
     try:
-        if file_path.is_dir():
-            print(f"Input data folder {file_path} already in place. "\
+        if _input_exists(case_input_dir_path):
+            print(f"Input data folder {case_input_dir_path} already in place. "\
             + "Make sure that it contains all the necessary forcing files!")
 
         else:
+
+            print(f"Downloading and extracting input for {case_input_dir_name}"\
+            + f"...", end="")
             # Create directory
-            file_path.mkdir(parents=True, exist_ok=True)
+            case_input_dir_path.mkdir(parents=True, exist_ok=True)
 
             ### Download the tar file from `url` and save it under `fname`:
-            fname = file_path / (file_name + ".tar")
+            fname = case_input_dir_path / (case_input_dir_name + ".tar")
             with urllib.request.urlopen(url) as response, open(fname, 'wb') \
             as out_file:
                 shutil.copyfileobj(response, out_file)
 
             ### Extract tar file
             with tarfile.open(fname) as f:
-                # do whatever
-                f.extractall(file_path)
+                f.extractall(case_input_dir_path)
 
-            print(f"Finished downloading and extracting {file_name}. \n")
+            print(f"Done!")
+
+            return(case_input_dir_path)
 
     except:
-        print("Error when downloading input data: \n")
+        print("\nError when downloading input data.\n")
         raise
 
+################################################################################
 
 ### Function to print site info in table format
 def _print_table_row(ind, name, res, lat, lon):
@@ -74,6 +170,7 @@ def print_cases(cases_df):
 
     print('---------------------------------------------------------------')
 
+################################################################################
 
 def check_dir(dir):
     '''
