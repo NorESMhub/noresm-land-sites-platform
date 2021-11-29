7b7fd1fb606ca79852f6747ae17c2bb9f0ead145
Eva Lieungh <39821980+evalieungh@users.noreply.github.com>
2021-11-29 13:47:18 +0100

    add plotting

--- landsites_tools/postprocess/postprocess.py
+++ landsites_tools/postprocess/postprocess.py
@@ -3,45 +3,47 @@
 """postprocess.py: post-process the CTSM simulation results, e.g. create plots
 for output variables defined in a settings file."""
 
-# Read in which output variables to plot from the settings file
-import sys
-sys.path.append('~/landsites_tools/inputprep/output_variables_subset.json')
-import output_variables_subset.json as vars
-
-
 import os
+import sys
 import xarray as xr
-xr.set_options(display_style="html") # is this necessary?
 import matplotlib.pyplot as plt
 
 # get the output files in the dir_cases and dir_output specified in settings file
+from settings.txt import dir_output
+from settings.txt import dir_cases
 path = os.path.join(dir_output, 'archive', dir_cases, 'lnd', 'hist')
 
 # read all the netCDF files available in the history folder.
-dset = xr.open_mfdataset(path + '/*.nc', combine='by_coords') # slow!
+# NB! Slow if there are many files!
+dset = xr.open_mfdataset(path + '/*.nc', combine='by_coords')
 
 # look at the data set
 dset
 
 # plot a subset of the model output variables
 #-----------------------------------------------
-"""add code for automatic plotting off/on"""
-# 2-dimensional plots with lines over time
-dset['ED_NCOHORTS'].plot(aspect=3, size=6)  # Total number of ED cohorts per site | unitless
-dset['ED_NPATCHES'].plot(aspect=3, size=6) # Total number of ED patches per site | unitless
-dset['ELAI'].plot(aspect=3, size=6) # exposed one-sided leaf area index | unit m2/m2
-# ... etc
+"""add code for automatic plotting off/on.
+if settings.txt/variables_plot = TRUE, run all plotting code,
+else print 'automated output plotting is OFF because settings.txt/variables_plot = FALSE' """
 
-# 3-dimensional plots with age or PFT classes
-dset['CANOPY_AREA_BY_AGE'].plot(aspect=3, size=6) # canopy area by age bin | units: m2/m2
-dset['LEAF_HEIGHT_DIST'].plot(aspect=3, size=6) # leaf height distribution | units: m2/m2
-dset['RECRUITMENT'].plot(aspect=3, size=6) # Rate of recruitment by PFT | units: indiv/ha/yr
+from settings.txt import variables_plot
+if variables_plot == TRUE :
+    #Read in which output variables to plot from the settings file]
+    sys.path.append('~/landsites_tools/inputprep/output_variables_subset.json')
+    import output_variables_subset.json as vars
 
-# to save a plot, use this (from Anne's tut) and change variable name
-p = dset['CANOPY_HEIGHT_DIST'].plot(aspect=3, size=6, col_wrap=1, col='fates_levheight')
-p.fig.savefig('CANOPY_HEIGHT_DIST.png') # where does this save to?
+    for var in vars:
+        dset[var].plot(aspect=3, size=6)
+else :
+    print('automated output plotting is OFF because settings.txt/variables_plot = FALSE')
 
+#to save a plot, use this (from Anne's tut) and change variable name
+p = dset['var'].plot(aspect=3, size=6, col_wrap=1, col='fates_levheight')
+p.fig.savefig('myplot.png') # where does this save to?
 
 # Model validation
 #-------------------------------------------------
+"""add code for automatic validation off/on.
+if settings.txt/model_validation = TRUE, run all validation code,
+else print 'automated model validation is OFF because settings.txt/model_validation = FALSE' """
 # Maybe this should be a separate script? Get inspiration from scripts by Charlie Koven
