2326d31a2115bc8b42ef2da656cc938fb6d6a785
Michal Torma <miso.torma@gmail.com>
2022-02-09 09:01:31 +0100

    Install pip deps properly

--- .machine/NREC_VM_setup/privileged/install_dependencies_user.sh
+++ .machine/NREC_VM_setup/privileged/install_dependencies_user.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/bash
 
 # Python 3 packages
-pip3 install --upgrade pip --user
-pip3 install -r requirements_pip.txt --user
-pip3 install git+https://github.com/esmci/sphinx_rtd_theme.git@version-dropdown-with-fixes --user
+python3 -m pip install --upgrade pip --user
+python3 -m pip install -r requirements_pip.txt --user
+python3 -m pip install git+https://github.com/esmci/sphinx_rtd_theme.git@version-dropdown-with-fixes --user
 
 # # Vim plugins
 # mkdir -p .vim/bundle
