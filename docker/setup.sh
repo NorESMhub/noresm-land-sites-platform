4af55f5a9f99ea8686f2f357508c98485d4f6cbc
kaveh <ka7eh@pm.me>
2022-03-04 10:29:56 +0100

    Fix permissions

--- docker/setup.sh
+++ docker/setup.sh
@@ -7,5 +7,3 @@ fi
 
 export DOCKER_COMPOSE_FILE
 echo "Using $DOCKER_COMPOSE_FILE"
-
-docker-compose -f ${DOCKER_COMPOSE_FILE} run --user root noresm_land_sites_platform chown -R user:user /home/user/NorESM_LandSites_Platform/data /home/user/NorESM_LandSites_Platform/landsites_tools/settings.txt /home/user/NorESM_LandSites_Platform/landsites_tools/custom_settings /home/user/NorESM_LandSites_Platform/notebooks
