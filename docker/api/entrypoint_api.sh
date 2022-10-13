a258b47a5197a6f85f17c231bb0b6452ae1fd75e
kaveh <ka7eh@pm.me>
2022-10-13 14:01:13 +0200

    Prepare for custom sites

--- docker/api/entrypoint_api.sh
+++ docker/api/entrypoint_api.sh
@@ -9,7 +9,9 @@ if [[ ! -d /ctsm-api/resources/ctsm ]]; then
 
     cd ctsm
 
-    git checkout 1728491743cc572981863764c13bad4949025590
+    git checkout 112f21c43507a5802014677be7b71bd660987d4c
+
+    cp ../overwrites/Externals.cfg .
 
     ./manage_externals/checkout_externals
 
@@ -23,7 +25,6 @@ sudo -s -E -u "$USER" bash <<EOF
 cd /ctsm-api
 
 ./scripts/migrations_forward.sh
-
 
 uvicorn app.main:app --host 0.0.0.0
 
